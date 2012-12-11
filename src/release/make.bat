f77d0c5c8c97e3d1abb8dfb793a3febbd606ff38
Rob <upcrob@server.fake>
2012-12-10 18:59:54 -0600

    Removed extraneous directory

--- src/release/make.bat
+++
@@ -1,5 +0,0 @@
-flex u.l
-flex upre.l
-bison -d u.y
-bison -d upre.y
-gcc -o u ulex.c uprelex.c u.tab.c upre.tab.c symboltable.c symbolstack.c functiontable.c parsetree.c compiler.c strutil.c stringtable.c list.c identifiertypes.c stringqueue.c prunefunctions.c
--- src/release/prunefunctions.h
+++
@@ -1,9 +0,0 @@
-/*
- * Code for pruning unused functions from the parse tree.
- */
-
-#include "functiontable.h"
-#include "parsetree.h"
-
-struct tree_node* pruneUnusedFunctions(function_table* fTable, struct tree_node* root);
-
